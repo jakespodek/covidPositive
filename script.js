fb793280f056fce2f60e461df9f2cadb1731c35c
Ilya-Marvin <ilya44.m@gmail.com>
2021-05-01 14:57:56 -0400

    date-issue-fixed

--- script.js
+++ script.js
@@ -39,4 +39,28 @@ fetch(url)
 
     })
 
+const today = new Date()
+    const yesterday = new Date(today)
+    yesterday.setDate(yesterday.getDate() - 1)
+    today.toDateString();
+    yesterday.toDateString();
+    console.log(`yesterday: ${yesterday}`);
+    // document.getElementById("dateInput").setAttribute("max", yesterday);
+
+
+
+
+
+function maxDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+    return [year, month, day].join('-');
+}
+console.log(maxDate(yesterday));
 
