580d1d5c7e3bbba69b6ea2aa9102dc006b5b8056
Jake Spodek <jake.spodek1@gmail.com>
2021-05-02 12:50:08 -0400

    refactored app

--- script.js
+++ script.js
@@ -18,54 +18,76 @@
     // STRETCH GOAL:
         // spread some positive vibes by also generating a "vaccinated" gif(s) from GIPHY, somewhere on the page
 
-let date = document.querySelector('input[type="date"]').value;
-console.log(date);
+const app = {};
+
+const date = document.querySelector('input[type="date"]');
 
 
 
-const url = new URL('https://api.opencovid.ca/summary')
+app.displayData = (data) => {
+    const ul = document.querySelector('.dataList');
 
-url.search = new URLSearchParams({
-    date: date
-});
-
-fetch(url)
-    .then((data) => {
-        return data.json();
+    data.forEach((item) => {
+        const li = document.createElement('li');
+        li.innerHTML = `<h2>${item.province}</h2>`
+        ul.append(li);
     })
 
-    .then((jsonResponse) => {
-        console.log(jsonResponse)
+}
 
+
+app.gimmeData = () => {
+    const url = new URL('https://api.opencovid.ca/summary')
+    const dateValue = date.value;
+
+    url.search = new URLSearchParams({
+        date: dateValue
+    });
+
+    fetch(url)
+        .then((data) => {
+            return data.json();
+        })
+
+        .then((jsonResponse) => {
+            console.log(jsonResponse)
+            app.displayData(jsonResponse.summary);
+        })
+    console.log(dateValue);
+}
+
+app.dateSelector = () => {
+    const form = document.querySelector('form');
+    date.max = app.yesterday();
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        app.gimmeData();
     })
+};
 
-// // const today = new Date()
-//     const yesterday = new Date()
-//     yesterday.setDate(yesterday.getDate() - 1)
-//     // today.toDateString();
-//     console.log(`yesterday: ${yesterday}`);
-//     yesterday.toDateString();
-//     // document.getElementById("dateInput").setAttribute("max", yesterday);
-
-
-
-
-
-yesterday = () => {
+app.yesterday = () => {
     const date = new Date()
+
     date.setDate(date.getDate() - 1)
     month = '' + (date.getMonth() + 1),
-    day = '' + date.getDate(),
-    year = date.getFullYear();
+        day = '' + date.getDate(),
+        year = date.getFullYear();
+
     if (month.length < 2)
         month = '0' + month;
     if (day.length < 2)
         day = '0' + day;
+
     return [year, month, day].join('-');
 }
 
-const maxDate = yesterday();
+console.log(`yesterday: ${app.yesterday()}`);
 
-console.log(maxDate);
-console.log(`yesterday: ${maxDate}`);
 
+app.init = () => {
+    console.log('init!');
+    app.dateSelector();
+}
+
+app.init();
